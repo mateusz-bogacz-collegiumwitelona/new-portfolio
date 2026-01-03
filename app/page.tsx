244f2b14669987cac5fe404c01df69c7738c4558
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2026-01-03 22:34:24 +0100

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-
+import { HeroSection } from "@/components/home-hero";
 export default function Home() {
   return (
     <div className="bg-[#1F2937]">
       <Navbar />
-      <main className="flex flex-col items-center justify-center min-h-screen  text-[#E5E7EB]"></main>
+      <HeroSection />
       <Footer />
     </div>
   );
